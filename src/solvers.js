902a1fca7fa43f692938ea616e8f3c167ae70184
Hack Reactor Students <accounts@hackreactor.com>
2015-07-07 12:10:25 -0700

    Implemented countNRooksSolutions recursively

--- src/solvers.js
+++ src/solvers.js
@@ -16,92 +16,63 @@
 window.findNRooksSolution = function(n) {
 
   var solution = new Board({n: n});
-  // Check if there are any row or column conflicts in each square
-  for (var i = 0; i < n; i++) {
-    // Loop through each row
-    for (var j = 0; j < n; j++) {
-      solution.attributes[i][j] = 1;
 
-      if (!solution.hasRowConflictAt(i) && !solution.hasColConflictAt(j)){
-        continue;
+  var placeRook = function(rowIndex){
+    // Recursive solution
+    for (var i = 0; i < n; i++) {
+      solution.togglePiece(rowIndex, i);
+      
+      if (solution.hasAnyRooksConflicts()) {
+        solution.togglePiece(rowIndex, i);
       } else {
-        solution.attributes[i][j] = 0;
-      }
+        if (rowIndex < n - 1) {
+          placeRook(rowIndex + 1);
+        }
+      } 
     }
-  }
+  };
+
+  placeRook(0);
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution.rows();
 };
 
 
-
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
+  
   var solutionCount = 0;
   var testBoard = new Board({n: n});
 
-  seedFirstRow(testBoard);
-
-  var seedFirstRow = function(board){
-    var randomIndex;
+  var placeRook = function(rowIndex){
     
-    // Only add a piece if one doesn't exist already
-    if (_.indexOf(board.attributes[0], 1) === -1) {
-      randomIndex = Math.floor(Math.random() * board.attributes.n);
-      board.attributes[0][randomIndex] = 1;
-    }
-  };
-
-  var firstRow = testBoard.attributes[0];
-
-  var decisionTree = new Tree();
-
-  tree.value = firstRow;
-  tree.children = getChildren(firstRow);
-
-  if (tree.children.length )
-
-  var getChildren = function(array) {
-    var children = [];
-    var n = array.length;
-    
-    if (n < 2) {
-      return children = null;
-    }
-
-    var parentIndex = null;
-    var insertAt = null;
-
-    // Discover where rook exists in passed array
     for (var i = 0; i < n; i++) {
-      if (array[i] === 1 && parentIndex === null) { parentIndex = i; }
-    }
-
-    // Generate empty rows based on passed array size
-    for (var i = 0; i < n - 1; i++) { children.push([]); }
-
-    var nextElement = parentIndex + 1;
-
-    // Iterate over new arrays and add 1s where applicable
-    for (var i = 0; i < n - 1; i++) {
-      // Set element back to zero if row size is exceeded
-      if (!(nextElement < n)) { nextElement = 0; }
-
-      // Go through each item in the row and decide if it's a good spot 
-      for (var j = 0; j < n; j++) {
-        children[i][j] = 0;
-        children[i][nextElement] = 1;
+      testBoard.togglePiece(rowIndex, i);      
+      
+      if (testBoard.hasAnyRooksConflicts()) {
+        testBoard.togglePiece(rowIndex, i);
+      } else {
+        if (rowIndex === n - 1 ) {
+          solutionCount++;
+          testBoard.togglePiece(rowIndex, i);
+          return;
+        }
+        if (rowIndex < n - 1) {
+          placeRook(rowIndex + 1);
+        }
+      } 
+      if (testBoard.attributes[rowIndex][i] === 1) {
+        testBoard.togglePiece(rowIndex, i);
       }
-      nextElement++;
     }
-    return children;
   };
+
+  placeRook(0);
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
-
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
@@ -119,61 +90,4 @@ window.countNQueensSolutions = function(n) {
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
-};
-
-// Copied from hackreactor 2015-06-data-structures/sprint-two/src/tree.js repo solution branch
-var Tree = function(value){
-  var newTree = {};
-  newTree.value = value;
-
-
-  extend(newTree, treeMethods);
-  newTree.children = [];
-
-  return newTree;
-};
-
-
-
-var extend = function(to, from){
-  for (var key in from){
-    to[key] = from[key];
-  }
-};
-
-
-var treeMethods = {};
-
-treeMethods.addChild = function(value){
-
-  var child = Tree(value);
-  this.children.push(child);
-};
-
-treeMethods.contains = function(target){
-
-  var found = false;
-  var subroutine = function(node){
-    if ( node.value === target ){
-      found = true;
-      return;
-    }
-    for ( var i = 0; i < node.children.length; i++ ){
-      var child = node.children[i];
-      subroutine(child);
-    }
-  }
-  subroutine(this);
-  return found;
-};
-
-treeMethods.traverse = function(callback){
-  callback(this.value);
-
-  if ( !this.children ){ return; }
-  for ( var i = 0; i < this.children.length; i++ ){
-    var child = this.children[i];
-    debugger;
-    child.traverse(callback);
-  }
 };
